0c0f4ef8497f718bb75272d2127d2c29953e73c0
frivolous-impulsor <ultrapilot95@gmail.com>
2024-03-31 16:34:33 -0400

    remove

--- finalLab1.py
+++ finalLab1.py
@@ -30,6 +30,30 @@ class IndexMinPQ:
         self.im[insertIndex] = key
         self.swim(insertIndex)
         self.size +=1
+    
+    def sink(self, i):
+        left = i * 2 + 1
+        right = left + 1
+        smaller = left
+        if right < self.size and self.val[left] > self.val[right]:
+            smaller = right
+        if smaller < self.size and self.val[self.im[i]] > self.val[self.im[smaller]]:
+            self.swap(i, smaller)
+            self.sink(smaller)  
+
+    def remove(self, key): 
+        removed = (key, self.val[key])
+        kPosition = self.pm[key]
+        endPosition = self.size-1
+        self.swap(kPosition,endPosition)
+        self.val[key] = None
+        self.pm[key] = None
+        self.im[endPosition] = None
+        self.size -=1
+        self.sink(kPosition)
+        return removed
+
+
 
 
 class TestIndexHeapTest(unittest.TestCase):
@@ -40,15 +64,44 @@ class TestIndexHeapTest(unittest.TestCase):
     def testInsert(self):
         heap = self.heap
         insertSize = 4
-        insertVals = [random.randint(1,100) for i in range(insertSize)]
+        insertVals = [10, 12, 5, 0]
+        insertPm = [2,3,1,0]
+        insertIm = [3,2,0,1]
 
         for i in range(insertSize):
             heap.insert(i,insertVals[i])
         expectedVal = [None for i in range(self.testSize)]
+        expectedPm = [None for i in range(self.testSize)]
+        expectedIm = [None for i in range(self.testSize)]
         for i in range(len(insertVals)):
             expectedVal[i] = insertVals[i]
+            expectedPm[i] = insertPm[i]
+            expectedIm[i] = insertIm[i]
         self.assertEqual(heap.val, expectedVal)
+        self.assertEqual(heap.pm, expectedPm)
+        self.assertEqual(heap.im, expectedIm)
+        self.assertEqual(heap.size, insertSize)
 
+    def testRemove(self):
+        heap = self.heap
+        insertSize = 4
+        insertVals = [10, 12, 5, 0]
+        insertPm = [0,2,1]
+        insertIm = [0,2,1]
+
+        for i in range(insertSize):
+            heap.insert(i,insertVals[i])
+        heap.remove(3)
+        expectedVal = [None for i in range(self.testSize)]
+        expectedPm = [None for i in range(self.testSize)]
+        expectedIm = [None for i in range(self.testSize)]
+        for i in range(len(insertPm)):
+            expectedVal[i] = insertVals[i]
+            expectedPm[i] = insertPm[i]
+            expectedIm[i] = insertIm[i]
+        self.assertEqual(heap.val, expectedVal)
+        self.assertEqual(heap.pm, expectedPm)
+        self.assertEqual(heap.im, expectedIm)
 
 
 
