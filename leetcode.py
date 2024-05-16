07f7d1151a29a418fc7006c8c431184dbfe4ef10
admin_li2177 <admin_li2177@ads.mcmaster.ca>
2024-05-16 15:49:22 -0400

    evaluateTree leet

--- leetcode.py
+++ leetcode.py
@@ -19,8 +19,33 @@ class Solution:
             else:
                 return (headS == headP or headP == '.') and aux(ps+1, pp+1)
         return aux(0, 0)
-        
+
+    
+    def evaluateTree(self, root: list[int]) -> bool:   
+        maxL = len(root)     
+        if maxL == 1:
+            return root[0]
+        def aux(mid):
+            leftIdx = mid*2 + 1
+            rightIdx = leftIdx + 1
+            vLeft = root[leftIdx]
+            vRight = root[rightIdx]
+            v = root[mid]
+            if v < 2:
+                return v
+            if vLeft < 2:
+                if v == 2:
+                    return vLeft or vRight
+                else:
+                    return vLeft and vRight
+            else:
+                if v == 2:
+                    return aux(leftIdx) or aux(rightIdx)
+                else:
+                    return aux(leftIdx) and aux(rightIdx)
+                
+        return aux(0)
+                    
         
     
 solution = Solution()
-print(solution.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*c"))
