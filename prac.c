e56e0e72494907e706c95d9a6ea01abffe7ce47d
frivolous-impulsor <ultrapilot95@gmail.com>
2024-06-22 17:54:32 -0400

    nothing

--- prac.c
+++ prac.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(){
-    printf("Hello C!\n");
-
-    return 0;
+    const double PI = 3.1415;
+    double radius;
+    double circumference;
+    printf("the radius of the circle is: ");
+    scanf("%lf", &radius);
+    circumference = 2 * radius * PI;
+    printf("the circumference of the circle is %lf\n", circumference);
 }
