f9ba1595c23985ac5daadf4fab80e36ca6d69c3b
frivolous-impulsor <ultrapilot95@gmail.com>
2024-06-25 16:34:27 -0400

    to pointer

--- prac.c
+++ prac.c
@@ -1,55 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-
-typedef char username[25];
-
-typedef struct 
-{
-    /* data */
-    username name;
-    int userId;
-}User;
-
-typedef struct
-{
-    /* data */
-    char name[25];
-    char major[25];
-}Student;
-
-
-
-void printArray(int arr[], int len){
-    for(int i = 0; i < len; i++){
-        printf("%d\n",arr[i]);
-    }
-}
-
-void bubbleSort(int array[], int len){
-
-    for(int i = 0; i < len-2; i++){
-        for(int j=0; j < len-1; j++){
-            if(array[j] > array[j+1]){
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-    }
-    printArray(array, len);
-}
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
-    Student s1 = {"batman", "material engineering"};
-    Student s2 = {"ironman", "mechanical engineering"};
-    Student s3 = {"hawk", "physics"};
 
-    Student students[3] = {s1, s2, s3};
+    int age = 20;
 
-    for(int i = 0; i < sizeof(students)/sizeof(students[0]); i++ ){
-        printf("%-18s", students[i].name);
-        printf("%-18s\n", students[i].major);
-    }
+    int *pAge = &age;
+
+    
+    printf("age value is: %d\n", age);
+    printf("value at address is : %d\n", *pAge);
+    printf("age address is: %p\n", &age);
+    printf("address of age is: %p\n", pAge);
     
     return 0;
 }
