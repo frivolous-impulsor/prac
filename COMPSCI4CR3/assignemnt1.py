59b1f95f80b051370ec9ec719b452c4ea0ef77ed
frivolous-impulsor <ultrapilot95@gmail.com>
2024-09-24 13:30:41 -0400

    generateKey init draft

--- COMPSCI4CR3/assignemnt1.py
+++ COMPSCI4CR3/assignemnt1.py
@@ -20,16 +20,31 @@ class LFSR:
             raise Exception("length of initVals must be the same as degree of LFSR")
         self.initVal = initVals
 
-    def generateKey(self, size: int):
-        if (size <= self.degree):
-            return self.p[:size]
-        key = [0 for _ in range(size)]
-        for i in range(len(self.initVal)):
+    def generateKey(self, kSize: int):
+        if (kSize <= self.degree):
+            return self.initVal[:kSize]
+        key = [0 for _ in range(kSize)]
+        for i in range(self.degree):
             key[i] = self.initVal[i]
         pointer: int = len(self.initVal)
+        lp: int = pointer - self.degree
+        rp: int = pointer-1
+        while pointer < kSize:
+            result = 0
+            while lp < rp:
+                result += self.p[lp % self.degree]*key[lp]
+                lp +=1
+            result = result % 2
+            key[pointer] = result
+            pointer +=1
+            lp = pointer - self.degree
+            rp = pointer-1
+        return key
         
 
-q1 = LFSR(6)
-q1.setP(q1.str2List("110101"))
+        
 
-print(q1.p)
+q1 = LFSR(20)
+q1.setP(q1.str2List("11010100010001101010"))
+q1.setInitVal(q1.str2List("00110110010101101111"))
+print(q1.generateKey(10000))
