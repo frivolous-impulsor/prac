cf0748aa6aaa3342d79646e52cb6e0d09496fb45
frivolous-impulsor <ultrapilot95@gmail.com>
2024-09-24 14:46:20 -0400

    encode and decode tested

--- COMPSCI4CR3/assignemnt1.py
+++ COMPSCI4CR3/assignemnt1.py
@@ -10,6 +10,11 @@ class LFSR:
         nums = [int(c) for c in strNums]
         return nums
     
+    def list2Str(self, nums: list[int]):
+        strList: list[str] = map(str, nums)
+        delimiter: str = ''
+        return delimiter.join(strList)
+    
     def setP(self, Ps: list[int]):
         if (len(Ps) != self.degree):
             raise Exception("length of Ps must be the same as degree of LFSR")
@@ -26,25 +31,45 @@ class LFSR:
         key = [0 for _ in range(kSize)]
         for i in range(self.degree):
             key[i] = self.initVal[i]
-        pointer: int = len(self.initVal)
+        pointer: int = self.degree
         lp: int = pointer - self.degree
+        mark: int = lp
         rp: int = pointer-1
         while pointer < kSize:
             result = 0
-            while lp < rp:
-                result += self.p[lp % self.degree]*key[lp]
+            while lp <= rp:
+                result += self.p[lp - mark]*key[lp]
                 lp +=1
             result = result % 2
             key[pointer] = result
             pointer +=1
             lp = pointer - self.degree
+            mark = lp
             rp = pointer-1
         return key
-        
+    
+    def encode(self, plainText: list[int]):
+        size = len(plainText)
+        key = self.generateKey(size)
+        cipherText: list[int] = [0 for _ in range(size)]
+        for i in range(size):
+            cipherText[i] = key[i] ^ plainText[i]
+        return cipherText
+    
+    def decode(self, cipherText: list[int]):
+        size = len(cipherText)
+        key = self.generateKey(size)
+        plainText: list[int] = [0 for _ in range(size)]
+        for i in range(size):
+            plainText[i] = key[i] ^ cipherText[i]
+        return plainText
 
-        
-
-q1 = LFSR(20)
-q1.setP(q1.str2List("11010100010001101010"))
-q1.setInitVal(q1.str2List("00110110010101101111"))
-print(q1.generateKey(10000))
+def test():
+    q1 = LFSR(20)
+    q1.setP(q1.str2List("11010100010001101010"))
+    q1.setInitVal(q1.str2List("00110110010101101111"))
+    cipher = q1.encode(q1.str2List("00111101011100111000000111001100010011010110110100"))
+    print(q1.decode(cipher))
+    print(q1.str2List("00111101011100111000000111001100010011010110110100"))
+    print(q1.list2Str(q1.str2List("00111101011100111000000111001100010011010110110100")))
+test()
