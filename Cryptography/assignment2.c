fe9918622e8aa1e5022276bcd9c702a39132436f
frivolous-impulsor <ultrapilot95@gmail.com>
2024-10-09 15:27:50 -0400

    start hashing process

--- Cryptography/assignment2.c
+++ Cryptography/assignment2.c
@@ -7,12 +7,11 @@
 int printStrInBinary(char *str, int size);
 int padding(char *str, int size, char *output);
 
-int rightRotate(int num, int iterations);
-
-int rightShift(int num, int iterations);
-
 int messageSchedule(char* padded, int len);
 
+
+int rightRotate(int num, int iterations);
+int rightShift(int num, int iterations);
 int Ch(int x, int y, int z);
 int Ma(int x, int y, int z);
 
@@ -22,23 +21,48 @@ int sig0(int x);
 int sig1(int x);
 
 
+
+
+int hashing(char* msg, int size);
+
 int main(int argc, char* argv[]){
+    char secret[] = "The quick brown fox jumped over the lazy dog.fasndjkfnwnbo;wbo;wb";
+    int size = strlen(secret);
+    hashing(secret, size);
+    
 
+    
 
-    int h0, h1, h2, h3, h4, h5, h6, h7;
-    h0 = 0x6a09e667;
-    h1 = 0xbb67ae85;
-    h2 = 0x3c6ef372;
-    h3 = 0xa54ff53a;
-    h4 = 0x510e527f;
-    h5 = 0x9b05688c;
-    h6 = 0x1f83d9ab;
-    h7 = 0x5be0cd19;
-    int a = 0x80000000;
-    printf("before %02x\n", a);
+    return 0;
+}
 
-    a = rightShift(a, 2);
-    printf("after %02x\n", a);
+int hashing(char* msg, int size){
+    //initializing the constants H and K
+    unsigned int H[] = {
+    0x6a09e667,
+    0xbb67ae85,
+    0x3c6ef372,
+    0xa54ff53a,
+    0x510e527f,
+    0x9b05688c,
+    0x1f83d9ab,
+    0x5be0cd19};
+
+    unsigned int K[] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    };
+
+    //preprocessing/padding the input msg
+    char padded[size + 512];
+    int paddedLen = padding(msg, size, padded)/8;
+    printStrInBinary(padded, paddedLen); 
 
     return 0;
 }
@@ -72,8 +96,6 @@ int padding(char *str, int size, char *output){
     for(j = 8; j >=1; j--){     //append the length of original str as a 8-byte integer
         res[totalSizeBit/8 - j] = (length64 >> (j-1)*8) & 0xff;
     }
-    printStrInBinary(res, totalSizeBit/8);
-    printf("\nseparate: \n");
     for(m = 0; m < totalSizeBit/8; m++){
         output[m] = res[m];
     }
